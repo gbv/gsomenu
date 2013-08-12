8d1fc9732ef7457181bd9616256aa78337f02885
Jakob Voss <voss@gbv.de>
2013-08-12 10:25:32 +0200

    Liste als CSV-Datei

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 gsmenu.json: menu.yaml databases.yaml
 	@echo "Updating gsomenu.json"
 	@./makemenu.pl
+
+alle-gsomenu-datenbanken.csv: gsomenu.json
+	@./json2csv.pl
---
+++ json2csv.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use v5.10;
+
+use JSON;
+use Text::CSV;
+my $db = from_json( do { local $/; <> } );
+
+my @rows = [qw(dbkey title_de title_en url)];
+
+walk ($db->{databases});
+
+sub walk {
+    for (@{$_[0]}) {
+        if ($_->{databases}) {
+            walk($_->{databases});
+        } else {
+            push @rows, [ $_->{dbkey}, $_->{title_de}, $_->{title_en}, $_->{access} ];
+        }
+    }
+}
+
+my $csv = Text::CSV->new( { eol => "\r\n" } );
+open my $fh, ">", "alle-gsomenu-datenbanken.csv";
+print $fh "\xEF\xBB\xBF"; # BOM
+$csv->print($fh, $_) for @rows;
+close $fh;
+
