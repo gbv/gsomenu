daaec395f956949411aa0803fe5233808d992ee9
Jakob Voss <voss@gbv.de>
2014-02-07 14:17:34 +0100

    fix pull

--- deploy.php
+++ deploy.php
@@ -61,7 +61,7 @@ flush();
 $commands = array(
 	'echo $PWD',
 	'whoami',
-	'git pull',
+	'git pull origin master',
 	'git status',
 #    'cpanm --local-lib=~/perl5 local::lib && eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)',
 #    'make deps',
